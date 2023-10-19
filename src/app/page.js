f21de3e4f4f750464011efb4b1e7e9574e71d8e1
EzeVieraRep <eviera32@gmail.com>
2023-10-19 18:31:52 -0300

    Contact Realizado

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import { Button } from "@nextui-org/react";
 import Header from "@/components/Header";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
    <>
     <Header></Header>
+    <Contact />
    </>
   )
 }
