2c577edf6275c44255e1fb7733ffc5511bf3bf72
EzeVieraRep <eviera32@gmail.com>
2023-10-24 17:02:21 -0300

    Changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
                         <h1 className='text-3xl md:text-5xl font-bold'>
                             Diseño de paginas web empresariales en Argentina</h1>
                         <h6 className="text-2xl font-light">
-                            Tienda online, aplicaciones de inventario, y productos personalizados</h6>
+                            Tienda online, aplicaciones de inventario y productos personalizados</h6>
                     </div>
 
                 </div>
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -7,13 +7,13 @@ import React from 'react'
 function Contact() {
   return (
     <Motion style={"bg-purple-900 h-fit flex flex-wrap justify-center pt-20 pb-20 z-100"} idMotion={"Contact"}>
-      <div className="presupuesto basis-1/2 py-5 px-1 flex flex-col gap-4 m-w-fit z-10">
+      <div className="presupuesto basis-1/2 py-5 px-1 flex flex-col gap-4 m-w-fit z-10 mr-10">
         <Image className='w-[150px]' src='https://www.hidalgotitlan.gob.mx/assets/imgs/presupuesto.png' alt='LogoPresupuesto'></Image>
         <h1 className='text-3xl font-black'>Presupuesto sin compromiso</h1>
         <p className='text-s my-1 md:w-4/5'>Tanto si estás pensando en la creación de una página web para tu negocio, como si queres dar un empujon a tu plan de marketing digital, en RENF podemos ayudarte.
           ¡Completa el formulario y nos pondremos en contacto contigo!</p>
       </div>
-      <div className="m-auto z-10">
+      <div className="m-auto flex ml-21 z-10">
         <FormContact/>
       </div>
     </Motion>
