e78ffafbe48fe1468ec37e0c4b06ac6fc6125592
kirinico <kirinico@gmail.com>
2023-10-24 20:14:10 -0300

    se cambio el lugar del boton

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,10 +6,11 @@ import { useState } from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 import useMotion from "@/hooks/useMotion";
+import { WhatsappButton } from "./WhatsappButton";
 
 const Layout = ({ children }) => {
     const { color, myRef } = useMotion()
-    
+
     return (
         <div className="animateme">
             <div className="bg-gradient-to-r from-indigo-300 from-20% to-purple-900 to-80% h-full" >
@@ -30,6 +31,7 @@ const Layout = ({ children }) => {
                     <Header color={color} />
                     <div ref={myRef}></div>
                     {children}
+                    <WhatsappButton phoneNumber="1122543976" />
 
                 </ul>
             </div>
